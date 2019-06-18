15a58a8123a1762f9b83f65563b0cd6407b1bcc9
Ivan Simakovich <FirstName_LastName@its.jnj.com>
2019-06-18 13:27:02 +0300

    Twig

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -6,10 +6,11 @@
  * Time: 5:37 PM
  */
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
   /**
    * @Route("/")
@@ -23,9 +24,14 @@ class ArticleController
    */
   public function show($slug)
   {
-    return new Response(sprintf(
-      'Future page to show the article: %s',
-      $slug
-      ));
+    $comments = [
+      'First generation: 1937 – 1946',
+      'Second generation: 1947 – 1962',
+      'Third generation: 1963 - present',
+    ];
+    return $this->render('article/show.html.twig', [
+      'title' => ucwords(str_replace('-', ' ', $slug)),
+      'comments' => $comments,
+    ]);
   }
 }
