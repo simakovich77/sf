5f72b7e002eb538ad64f76f64d82784690eead16
FirstName LastName <FirstName_LastName@its.jnj.com>
2019-07-01 16:43:29 +0300

    KnpMarkdownBundle & its Services

--- the_spacebar/config/bundles.php
+++ the_spacebar/config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
 ];
