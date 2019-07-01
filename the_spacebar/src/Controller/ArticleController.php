1051bbbdc813975ebe091d585b874f7cb0cd2b69
FirstName LastName <FirstName_LastName@its.jnj.com>
2019-07-01 18:57:41 +0300

    The Cache Service

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -6,8 +6,10 @@
  * Time: 5:37 PM
  */
 namespace App\Controller;
+use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -24,16 +26,46 @@ class ArticleController extends AbstractController
   /**
    * @Route("/news/{slug}", name="article_show")
    */
-  public function show($slug)
+  public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
   {
     $comments = [
       'First generation: 1937 – 1946',
       'Second generation: 1947 – 1962',
       'Third generation: 1963 - present',
     ];
+
+    $articleContent = <<<EOF
+
+Eniac Computer
+The **first substantial** computer was the giant 
+ENIAC machine by John W. Mauchly and J. Presper Eckert 
+at the University of Pennsylvania. ENIAC (Electrical Numerical 
+Integrator and [Calculator](https://www.drupal.org/)) used a word of 10 decimal digits instead 
+of binary ones like previous automated calculators/computers. ENIAC 
+was also the first machine to use more than 2,000 vacuum tubes, using 
+nearly 18,000 **vacuum tubes**. Storage of all those vacuum tubes and the 
+machinery required to keep the cool took up over 167 square meters 
+(1800 square feet) of floor space. Nonetheless, it had punched-card 
+input and output and arithmetically had 1 multiplier, 1 divider-square 
+rooter, and 20 adders employing decimal "ring counters," which served as 
+adders and also as quick-access (0.0002 seconds) read-write register storage.
+EOF;
+
+    $item = $cache->getItem('markdown_'.md5($articleContent));
+
+    if(!$item->isHit()){
+        $item->set($markdown->transform($articleContent));
+        $cache->save($item);
+    }
+
+    $articleContent = $item->get();
+
+    //$articleContent = $markdown->transform($articleContent);
+
     return $this->render('article/show.html.twig', [
       'title' => ucwords(str_replace('-', ' ', $slug)),
       'slug' => $slug,
+      'articleContent' => $articleContent,
       'comments' => $comments,
     ]);
   }
