cd8e6ee0063e1a317330d1cca6e8c14ce3979196
Ivan Simakovich <FirstName_LastName@its.jnj.com>
2019-07-12 18:29:23 +0300

    s entities

---
+++ the_spacebar/src/Controller/ArticleAdminController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new")
+     */
+    public function new()
+    {
+        $article = new Article();
+        $article->setTitle('Why Asteroids Taste Like Bacon')
+            ->setSlug('why-asteroids-taste-like-bacon'.rand(100, 999))
+            ->setContent(
+                
+            );
+        return new Response('space rocks... added new');
+    }
+
+}
